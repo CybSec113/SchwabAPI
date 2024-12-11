588705fa975b602d0ba8be428ece44c42e930cf4
CybSec113 <71106462+CybSec113@users.noreply.github.com>
2024-12-11 17:03:03 -0500

    Implemented new and refresh token authentication Implemented basic quote, history and chains commands

--- auth.py
+++ auth.py
@@ -2,48 +2,84 @@ import requests
 import os
 import base64
 import pyperclip
+import json
 from dotenv import load_dotenv
+from crontab import CronTab
 
 load_dotenv()
 
-# set/get static data
+# global parameters
 api_key = os.getenv("SCHWAB_API_KEY")
 api_secret = os.getenv("SCHWAB_API_SECRET")
-auth_url = f"https://api.schwabapi.com/v1/oauth/authorize?client_id={api_key}&redirect_url=https://127.0.0.1"
-pyperclip.copy(auth_url)
-
-# authorization requires logging in with standard (i.e., not developer) credentials in browser
-print(f"Authorization URL (copied to clipboard): {auth_url}")
-
-# browser will return a url with the code; user must copy it here
-print("Enter returned URL:")
-returned_url = input()
-
-# extract the authentication response code from the url
-response_code = f"{returned_url[returned_url.index('code=')+5: returned_url.index('%40')]}@"
-
-# urlencode to base64
-creds = f"{api_key}:{api_secret}"
-b64_creds = base64.b64encode(creds.encode("utf-8")).decode("utf-8")
+token_url = "https://api.schwabapi.com/v1/oauth/token"
+auth_url = f"https://api.schwabapi.com/v1/oauth/authorize?client_id={api_key}&redirect_uri=https://127.0.0.1"
 
 # headers for token POST request
 headers = {
-    "Authorization": f"Basic {b64_creds}",
+    "Authorization": f"Basic {base64.b64encode(bytes(f"{api_key}:{api_secret}", "utf-8")).decode("utf-8")}",
     "Content-Type": "application/x-www-form-urlencoded",
 }
 
-# payload for token POST request
-payload = {
-    "grant_type": "authorization_code",
-    "code": response_code,
-    "redirect_uri": "https://127.0.0.1"
-}
+def makeRequest(url, payload, verb='post'):
+    if verb == "post":
+        return requests.post(url=url, headers=headers, data=payload)
+    elif verb == "get":
+        print("Not implemented, yet!")
 
-# the toke POST request
-response = requests.post(
-    url="https://api.schwabapi.com/v1/oauth/token",
-    headers=headers,
-    data=payload,
-)
+def getNewToken():
+    # copy url to clipboard: user must authenticate via browser with account (not dev) creds
+    pyperclip.copy(auth_url)
+    print(f"Authorization URL (copied to clipboard): {auth_url}")
 
-tokens_dict = response.json()
+    # following authentication, browser will return a url with the code; user must copy it here
+    print("Enter returned URL:")
+    returned_url = input()
+
+    # extract the authentication response code from the url
+    response_code = f"{returned_url[returned_url.index('code=')+5: returned_url.index('%40')]}@"
+
+    # payload for token POST request
+    payload = {
+        "grant_type": "authorization_code",
+        "code": response_code,
+        "redirect_uri": "https://127.0.0.1"
+    }
+
+    # the token POST request
+    response = makeRequest(token_url, payload)
+    print(response)
+    with open("token.json", "w") as f:
+        f.write(response.text)
+
+def getRefreshToken():
+    with open("token.json", "r") as f:
+        auth_text = f.read()
+    auth_json = json.loads(auth_text)
+
+    payload = {
+        "grant_type": "refresh_token",
+        "refresh_token": auth_json["refresh_token"],
+    }
+
+    response = makeRequest(token_url, payload)
+    print(response)
+    with open("token.json", "w") as f:
+        f.write(response.text)
+
+if __name__ == "__main__":
+    print("1 - New token")
+    print("2 - Refresh token")
+    print("3 - Set token timer")
+    command = input("Selection: ")
+
+    if command == '1':
+        getNewToken()
+    elif command == '2':
+        getRefreshToken()
+    elif command == '3':
+        cron = CronTab(user=True)
+        cron.remove_all()
+        cron.write()
+        job = cron.new(command="say 'token'")
+        job.setall('29,59 * * * *')
+        cron.write()
--- market_data.py
+++ market_data.py
@@ -1,24 +1,34 @@
 import requests
 import datetime
 import time
+import json
 
 server="https://api.schwabapi.com/marketdata/v1"
 
-headers={
-    "accept": "application/json",
-    "Authorization": "Bearer I0.b2F1dGgyLmNkYy5zY2h3YWIuY29t.OPBdCNmLXMxsKIzfJV25SPDRiZNnexlQu5ZacdFJ3lE@",
-}
+def getHeaders():
+    with open("token.json", "r") as f:
+        auth_text = f.read()
+    auth_json = json.loads(auth_text)
+    bearer_token = auth_json["access_token"]
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {bearer_token}",
+    }
+    return headers
 
-def get_quote(symbol):
-    response = requests.get(f"{server}/{symbol}/quotes", headers=headers)
-
+def getQuote(symbol):
+    # optional payload values: quote, fundamental, extended, reference, regular
+    # omitting payload returns full response.
+    payload = {
+        "fields": "quote"
+    }
+    response = requests.get(f"{server}/{symbol}/quotes", headers=getHeaders(), params=payload)
     print(response)
-    print(response.text)
-    mark_index=response.text.index("mark")
-    mark=f"{response.text[mark_index+6:response.text.index(",", mark_index)]}"
-    print(f"Mark: {mark}")
+    with open("quote.json", "w") as f:
+        f.write(response.text)
+    print(json.dumps(response.json(), indent=4))
 
-def get_history(symbol):
+def getHistory(symbol):
     start_date=datetime.date(2024,10,1)
     end_date=datetime.date(2024,10,31)
 
@@ -34,11 +44,34 @@ def get_history(symbol):
         "needPreviousClose": False,
     }
 
-    response = requests.get(f"{server}/pricehistory", headers=headers, params=payload)
-    print(response)
-    print(response.text)
+    response = requests.get(f"{server}/pricehistory", headers=getHeaders(), params=payload)
+    with open("history.json", "w") as f:
+        f.write(response.text)
 
+def getChains(symbol):
+    payload = {
+        "symbol": symbol,
+        "contractType": "PUT",              # CALL, PUT, ALL
+        #"strikeCount": 10,                  # num of strikes above/below ATM
+        "includeUnderlyingQuote": False,
+        "strategy": "SINGLE",               # SINGLE (default), ANALYTICAL, COVERED, VERTICAL, CALENDAR
+                                            # STRANGLE, STRADDLE, BUTTERFLY, CONDOR, DIAGONAL, COLLAR, ROLL
+                                            # ANALYTICAL requires additional fields not included here
+        #"interval": 5.0,                    # strike interval for strategy chains
+        #"strike": 115.0,
+        #"range": "NTM",                     # ITM, NTM (near the money), OTM
+        #"fromDate": "2024-01-01",
+        #"toDate": "2024-03-01",
+        #"expMonth": "JAN",                  # JAN, FEB, etc...
+        #"optionType": "",                   # not documented
+    }
+    response = requests.get(f"{server}/chains", headers=getHeaders(), params=payload)
+    with open("chains.json", "w") as f:
+        f.write(response.text)
+    print(json.dumps(response.json(), indent=4))
 
 if __name__ == "__main__":
-    #get_quote('C')
-    get_history('C')
+    symbol = input("Symbol:")
+    getQuote(symbol)
+    #getHistory(symbol)
+    #getChains(symbol)
